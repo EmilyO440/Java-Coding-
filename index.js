760f5e82f69ee8ae4dbfaf121622fc8228e2e94f
EmilyO440 <147689465+EmilyO440@users.noreply.github.com>
2023-10-26 10:28:16 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('open Class Introduction');
+
+//single line comment
+
+/*
+Question 2: Addition Operation
+
+You have been given two variables, num1 and num2, each with predefined values as test cases. Your task is to calculate the sum of these variables and display the result using the console.log()
+*/
+
+var num1 = 10;
+var num2 = 2;
+
+//add the variables and display using console.log
+console.log(num1 + num2);
